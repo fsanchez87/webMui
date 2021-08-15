051f933e63f4f146c1f540250606da8f3353f0db
Francisco Sánchez <sanchezgarridof@gmail.com>
2021-08-15 18:56:38 +0200

    header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { makeStyles } from "@material-ui/core/styles";
 import { CssBaseline } from "@material-ui/core/";
+import Header from "./components/Header";
 
 function App() {
   const classes = useStyles();
   return (
     <div className={classes.root}>
       <CssBaseline />
+      <Header />
     </div>
   );
 }
